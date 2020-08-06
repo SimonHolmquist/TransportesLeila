641aec87bdd8bfce9c2eca908dc194c9fce9cf03
Simón Holmquist <simon.holmquist@gmail.com>
2020-08-06 01:37:51 -0300

    funcionando pero feo

---
+++ TransportesLeila/Models/CommentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransportesLeila.Models
+{
+    public class CommentModel
+    {
+        private int id;
+        private string author;
+        private string text;
+
+        public int Id { get => id; set => id = value; }
+        public string Author { get => author; set => author = value; }
+        public string Text { get => text; set => text = value; }
+    }
+}
